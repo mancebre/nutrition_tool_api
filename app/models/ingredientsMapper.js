b02d34ad9a4adca731510cfbb06002f4904f44d5
mance_bre <dj.mancovic@gmail.com>
2014-10-08 14:34:56 +0200

    ingredient search

--- app/models/ingredientsMapper.js
+++
@@ -1,9 +0,0 @@
-var mongoose     = require('mongoose');
-var Schema       = mongoose.Schema;
-
-var IngredientsMapperSchema   = new Schema({
-    name: String,
-    mysqlId: Number
-});
-
-module.exports = mongoose.model('IngredientsMapper', IngredientsMapperSchema);

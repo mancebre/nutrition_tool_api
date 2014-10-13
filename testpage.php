170304891c4043a7ee0b11444b1727ea1bf3c176
mance_bre <dj.mancovic@gmail.com>
2014-10-13 22:27:38 +0200

    test page

--- testpage.php
+++ testpage.php
@@ -9,6 +9,14 @@
   <script>
   </script>
   <style>
+    .left {
+      float: left;
+      margin-left: 500px;
+    }
+    .right {
+      float: right;
+      margin-right: 500px;
+    }
     textarea { width:500px; height: 250px;}
     .red {
       background-color: red;
@@ -20,69 +28,96 @@
   </style>
 </head>
 <body>
- <div id="recipeTextarea">
-  <form action="/" id="recipeForm">
-    <textarea id="rec"></textarea>
-    <input type="submit" value="Submit">
-  </form>
+<div class="left">
+   <div id="recipeTextarea">
+    <form action="/" id="recipeForm">
+      <textarea id="rec"></textarea>
+      <input type="submit" value="Submit">
+    </form>
+  </div>
+  <div id="result" style="display:none;">
+    <table id="ingredientsTable"></table>
+    <button onclick="openTextarea();">Edit ingredients</button>
+  </div>
 </div>
-<div id="result" style="display:none;">
-  <table id="ingredientsTable"></table>
-  <button onclick="openTextarea();">Edit ingredients</button>
+
+<div class="right">
+  <table id="calculationResult" style="display:none;">
+    <tr>
+      <th>Calculation</th>
+    </tr>
+  </table>
 </div>
 
 <script>
 // Attach a submit handler to the form
 $( "#recipeForm" ).submit(function( event ) {
-
-  var host = location.host;
- 
   // Stop form from submitting normally
   event.preventDefault();
- 
-  // Get some values from elements on the page:
-  var $form = $( this ),
-    term = $("#rec").val();
-    //url = 'http://104.131.17.237:8080/api/recipecheck';
-    url = 'http://'+host+':8080/api/recipecheck';
- 
-  $.ajax({
 
-    url : url,
+  calculate();
+});
 
-    type: 'POST',
+  function calculate() {
 
-    dataType : "json",
+    var host = location.host;
 
-    data: {ingredients: term},
+    // Get some values from elements on the page:
+    var $form = $( this ),
+      term = $("#rec").val();
+      if (term.length < 1) {
+        return false;
+      }
+      //url = 'http://104.131.17.237:8080/api/recipecheck';
+      url = 'http://'+host+':8080/api/recipecheck';
+   
+    $.ajax({
 
-    success:function(data) {
-      var table = '<tr><th>Amount</th><th>Measure</th><th>Ingredient</th><th>Delete</th></tr>';
-      $("#recipeTextarea").hide();
-      $("#result").show();
-          $.each( data.ingredients, function( i, val ) {
-            table += '<tr id="'+i+'">';
-            table += '<td ' + (val.amount == null ? 'class="red"' : "") + ' id="'+i+'_amount">'+val.amount+'</td> ';
-            table += '<td ' + (val.measure == null ? 'class="red"' : "") + ' id="'+i+'_measure">'+val.measure+'</td> ';
-            table += '<td ' + (val.ingredient == null ? 'class="red ingr" onClick="popup(this);" ' : 'title="'+val.ingredient+'"') + ' id="'+i+'_ingredient">'+val.keyword.replace(val.amount + ' ' + val.measure, '').trim()+'</td> ';
-            table += '<td class="red" onClick="removeRow('+i+');"> X</td>';
-            table += '</tr>';
-            console.log(val);
-      });
-      $("#ingredientsTable").html(table);
-          console.log(data);
-    },
+      url : url,
+
+      type: 'POST',
+
+      dataType : "json",
+
+      data: {ingredients: term},
+
+      success:function(data) {
+        var table = '<tr><th>Amount</th><th>Measure</th><th>Ingredient</th><th>Delete</th></tr>';
+        $("#recipeTextarea").hide();
+        $("#result").show();
+        $.each( data.ingredients, function( i, val ) {
+          table += '<tr id="'+i+'">';
+          table += '<td ' + (val.amount == null ? 'class="red"' : "") + ' id="'+i+'_amount">'+val.amount+'</td> ';
+          table += '<td ' + (val.measure == null ? 'class="red"' : "") + ' id="'+i+'_measure">'+val.measure+'</td> ';
+          table += '<td ' + (val.ingredient == null ? 'class="red ingr" onClick="popup(this);" ' : 'title="'+val.ingredient+'"') + ' id="'+i+'_ingredient">'+val.keyword.replace(val.amount + ' ' + val.measure, '').trim()+'</td> ';
+          table += '<td class="red" onClick="removeRow('+i+');"> X</td>';
+          table += '</tr>';
+        });
+        $("#ingredientsTable").html(table);
+
+        table = '';
+        $.each( data.recipeSum, function( i, val ) {
+          if (val > 0) {
+            table += '<tr>';
+            table += '<td>' + i.replace("_", " ") + '</td>';
+            table += '<td>' + val + '</td>';
+            table += '<tr>';
+          }
+
+          $("#calculationResult").html(table).show();
+        });
+      },
 
 
-    error: function (xhr, ajaxOptions, thrownError) {
-      console.log(xhr.responseText);
-      console.log(thrownError);
+      error: function (xhr, ajaxOptions, thrownError) {
+        console.log(xhr.responseText);
+        console.log(thrownError);
+      }
+
     }
 
+    );
   }
-
-);
-});
 
   function ingredientsSearch() {
     var id = $("#dialog input[name='id']").val();
@@ -104,18 +139,36 @@ $( "#recipeForm" ).submit(function( event ) {
   }
 
   function replace(id, newVal) {
-//    $("#"+id).text(newVal);
     var idSplit = id.split("_");
-    var line = idSplit[0];
-    var text = $("#rec").val().split("\n").filter(function(e){return e});
-    console.log(text[line], $( "#dialog input[name='ingredient']" ).val(), newVal);//TODO nadji i zameni i u textarea i u tabeli i ponovi kalkulaciju
+    var line = idSplit[0]; // line number in text area
+    var text = $("#rec").val().split("\n").filter(function(e){return e}); // text from textarea
+    var amount = $("#"+ line + "_amount").text();
+    var measure = $("#"+ line + "_measure").text();
+
+    text[line] = amount + ' ' + measure + ' ' + newVal;
+    text = text.join("\n");console.log(text);
+
+    $("#rec").val(text);
+    $("#"+id).text(newVal);
+
+    calculate();
+
   }
 
-  function removeRow(id) {//TODO obrisi i is textarea i iz tabele i ponovi kalkulaciju
+  function removeRow(id) {
+    var line = id; // line number in text area
+    var text = $("#rec").val().split("\n").filter(function(e){return e}); // text from textarea
+
+    text[line] = '';
+    text = text.join("\n");console.log(text);
+
+    $("#rec").val(text);
     $("#"+id).remove();
+
+    calculate();
   }
 
-  function popup(element) {console.log(element)
+  function popup(element) {// TODO we need measure corection also!!!
     $("#dialog #searchResult").empty();
     $( "#dialog input[name='id']" ).val(element.id);
     $( "#dialog input[name='ingredient']" ).val(element.innerHTML);
@@ -136,7 +189,7 @@ $( "#recipeForm" ).submit(function( event ) {
         .replace(/>/g, '&gt;');
   }
 </script>
- <div id="dialog" title="Basic dialog" style="display: none">
+ <div id="dialog" title="Ingredient Search" style="display: none">
    <input type="text" name="ingredient">
    <input type="hidden" name="id">
    <button onClick="ingredientsSearch();" name="ingredientSearch">Search</button>
